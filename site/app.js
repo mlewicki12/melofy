326b5f0020644d823216bff10e57c2d56a6a2150
DESKTOP-F4LL9NV\Leander <learexjey@gmail.com>
2019-06-05 14:32:54 -0400

    Completed issue #11, endpoint to insert rows into database

--- site/app.js
+++ site/app.js
@@ -118,6 +118,14 @@ app.get('/recommendations', function(req, res) {
 	spotify.recommendations(req.query.track);
 });
 
+app.get('/insertplaylist', function(req, res) {
+	var pLink = req.query.pLink;
+	var userid = req.query.userid;
+	con.query("INSERT INTO playlists (link, userid) values ('" + pLink + "', '" + userid + "');");
+	res.end();
+	
+});
+
 app.listen(8080, function() {
   console.log('Started listening on port 8080');
 });
